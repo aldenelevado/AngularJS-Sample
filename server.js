de769b07611355e99df4c8fde4f1ecc899e971a1
aldenelevado <alden.elevado@gmail.com>
2017-03-01 13:25:52 +0800

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	serveIndex = require('serve-index'),
+    app = express(),
+    port = 8080;
+
+app.use(serveIndex(__dirname,{'icons': true}));
+app.use(express.static(__dirname));
+
+app.listen(port)
+console.log("Server running at port " + port + "...\n");
+console.log("Browse: http://localhost:" + port);
